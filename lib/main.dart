024849cafb6ef7b3ca0888f46aa5987f78dead0c
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-08-17 21:59:43 +0100

    Updating Main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,22 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const MaterialApp(
+    home: Arrival(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
+class Arrival extends StatefulWidget {
+  const Arrival({super.key});
+
+  @override
+  State<Arrival> createState() => _ArrivalState();
+}
+
+class _ArrivalState extends State<Arrival> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
