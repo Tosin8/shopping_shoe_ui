fef3fab4b1ca62ebae0039e6fb134dd13bc652f8
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-08-17 23:35:50 +0100

    Updating Drawer

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,41 @@ class Arrival extends StatefulWidget {
 class _ArrivalState extends State<Arrival> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text(
+          'New Arrival',
+          style: TextStyle(
+              fontSize: 15,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 1.5,
+              color: Colors.black),
+        ),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.shopping_cart),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: const Icon(Icons.sort),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      drawer: Drawer(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 15.0, horizontal: 40.0),
+          child: ListView(
+            children: [
+              Image.asset(
+                'assets/images/icons/app_icon/app_icon.png',
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
